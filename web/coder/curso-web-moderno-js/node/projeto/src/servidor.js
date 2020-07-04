27458c562e26ab72a5024738205fbfe0ee29bb5d
Matheus Bronca <dev@matheusbronca.com>
2020-07-04 14:41:01 -0300

    added node curso js-moderno

---
+++ web/coder/curso-web-moderno-js/node/projeto/src/servidor.js
@@ -0,0 +1,36 @@
+const porta = 3003;
+
+const express = require('express');
+const app = express();
+const bancoDeDados = require('./bancoDeDados');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get((req, res, next) => {
+  res.send(bancoDeDados.getProdutos()); //Coverte para um OBJETO p/ JSON automaticamente
+});
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id));
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto); // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto); // JSON
+})
+app.listen(porta, () => {
+  console.log(`Servidor está executando na porta ${porta}.`);
+});
